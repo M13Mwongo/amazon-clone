cc34a0f53cd075e1fc51172bccb5da94a746239b
M13Mwongo <62349438+M13Mwongo@users.noreply.github.com>
2022-02-23 01:54:38 +0300

    Initialization
    Beginning files when setting up the project. Boilerplate react code is still in. Firebase has been set up but not initialized. Firebase config file created.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlTl2GNT7BRJupeu59oHs8WXnftsO-VmI",
+  authDomain: "ama-zonclone-230222.firebaseapp.com",
+  projectId: "ama-zonclone-230222",
+  storageBucket: "ama-zonclone-230222.appspot.com",
+  messagingSenderId: "947886243766",
+  appId: "1:947886243766:web:7d7e8f963290630305cb17",
+  measurementId: "G-L4QWPN6YTL"
+};
